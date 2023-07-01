d8effeaa437d89c98bbcbb46d8283ca495e2e631
Mihail Kiyamov <mihailkiyamov@yandex.ru>
2023-07-02 04:51:45 +0500

    Sprint15-Try1

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().optional().min(2).max(30),
-    avatar: Joi.string().optional(),
-    about: Joi.string().min(2).max(30),
-  }),
+  }).unknown(true),
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-  }),
+    name: Joi.string().optional().min(2).max(30),
+    avatar: Joi.string().optional(),
+    about: Joi.string().optional().min(2).max(30),
+  }).unknown(true),
 }), createUser);
 
 app.use(auth);
