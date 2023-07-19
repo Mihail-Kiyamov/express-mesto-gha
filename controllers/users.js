c5647da0f1e142a585d674fb720c2c95250c8ad4
Mihail Kiyamov <mihailkiyamov@yandex.ru>
2023-07-19 13:51:34 +0500

    try

--- controllers/users.js
+++ controllers/users.js
@@ -67,7 +67,6 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.patchUser = (req, res, next) => {
   const { name, about } = req.body;
-
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
     .catch(next);
