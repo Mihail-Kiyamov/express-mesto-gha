fbbf18d04ceeea9040727eb8976b6b437101ddd0
Mihail Kiyamov <mihailkiyamov@yandex.ru>
2023-07-23 15:02:11 +0500

    sprint15

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 * 24 * 7 }).send({ data: user });
+      res.cookie('jwt', token, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 604800000 }).send({ data: user });
     })
     .catch(next);
 };
